28fb58432c2a07004a624db5899ee547db5978fc
Tarun <tarunsagwal38@gmail.com>
2024-08-05 13:36:53 +0530

    left and right join, delete btn and verification page is in process

--- signup.php
+++ signup.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
   require_once "config.php";
   if(isset($_POST['sub'])){
     $name = $_POST['fname'];
@@ -10,15 +11,38 @@
     $upass = substr($password,0,2);
     $unum = substr($num,0,2);
     $username = $uname.$umail.$upass.$unum;
-    $query = mysqli_query($con, "insert into users (fname, email, Mobile, password) values ('$name','$email','$num','$password')");
-    $table_query = mysqli_query($conn, "CREATE TABLE IF NOT EXISTS $username (
-        id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        data TEXT NOT NULL
-    )");
-    if($query && $table_query){
-     header('location: index.php'); 
-    }
+    if(isset($_POST['sub'])){
+    
+    // $_SESSION['user_table'] = $username;
+    // $query = mysqli_query($con, "insert into users (name, email, Mobile, password, username) values ('$name','$email','$num','$password', '$username')");
+    // $table_query = mysqli_query($conn, "CREATE TABLE IF NOT EXISTS $username (
+    //     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    //     data TEXT NOT NULL
+    // )");
+  
+?>
+<form class="otp-Form">
+ 
+  <span class="mainHeading">Enter OTP</span>
+  <p class="otpSubheading">We have sent a verification code to your mobile number</p>
+  <div class="inputContainer">
+   <input required="required" maxlength="1" type="text" class="otp-input" id="otp-input1">
+   <input required="required" maxlength="1" type="text" class="otp-input" id="otp-input2">
+   <input required="required" maxlength="1" type="text" class="otp-input" id="otp-input3">
+   <input required="required" maxlength="1" type="text" class="otp-input" id="otp-input4"> 
+   
+  </div>
+   <button class="verifyButton" type="submit">Verify</button>
+     <button class="exitBtn">×</button>
+     <p class="resendNote">Didn't receive the code? <button class="resendBtn">Resend Code</button></p>
+     
+</form>
+
+
+
+<?php
   }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -27,6 +51,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="css/signup.css">
+    <link rel="stylesheet" href="css/verification.css">
 </head>
 <body>
     <div class="form-box">
