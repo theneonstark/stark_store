6f106785894f7cd12f7426df9263ae2bebd72bbc
Mohd Shahid <mohd47149@gmail.com>
2024-08-11 02:37:23 +0530

    wishlist complete

--- index.php
+++ index.php
@@ -342,6 +342,11 @@ if (isset($_SESSION['email']) && isset($_SESSION['password'])) {
 
 				<div class="header-cart-content flex-w js-pscroll">
 					<ul class="header-cart-wrapitem w-full">
+						<?php
+							$wish_user = $_SESSION['wishlist'];
+							$wish_details = mysqli_query($wishlist_info, "SELECT * FROM wishlist.$wish_user uw JOIN product.product_item pi ON uw.wp_detail = pi.id WHERE uw.wp_detail AND pi.id ");		
+							while($wish_fetch = mysqli_fetch_assoc($wish_details)){
+							?>
 						<li class="header-cart-item flex-w flex-t m-b-12">
 							<div class="header-cart-item-img">
 								<img src="images/item-cart-01.jpg" alt="IMG">
@@ -349,46 +354,17 @@ if (isset($_SESSION['email']) && isset($_SESSION['password'])) {
 
 							<div class="header-cart-item-txt p-t-8">
 								<a href="#" class="header-cart-item-name m-b-18 hov-cl1 trans-04">
-									White Shirt Pleat
+									<?php echo $wish_fetch['product_name'];?>
 								</a>
-
+								
 								<span class="header-cart-item-info">
-									1 x $19.00
+									<?php echo $wish_fetch['product_price'];?>
 								</span>
 							</div>
 						</li>
-
-						<li class="header-cart-item flex-w flex-t m-b-12">
-							<div class="header-cart-item-img">
-								<img src="images/item-cart-02.jpg" alt="IMG">
-							</div>
-
-							<div class="header-cart-item-txt p-t-8">
-								<a href="#" class="header-cart-item-name m-b-18 hov-cl1 trans-04">
-									Converse All Star
-								</a>
-
-								<span class="header-cart-item-info">
-									1 x $39.00
-								</span>
-							</div>
-						</li>
-
-						<li class="header-cart-item flex-w flex-t m-b-12">
-							<div class="header-cart-item-img">
-								<img src="images/item-cart-03.jpg" alt="IMG">
-							</div>
-
-							<div class="header-cart-item-txt p-t-8">
-								<a href="#" class="header-cart-item-name m-b-18 hov-cl1 trans-04">
-									Nixon Porter Leather
-								</a>
-
-								<span class="header-cart-item-info">
-									1 x $17.00
-								</span>
-							</div>
-						</li>
+						<?php
+							}
+						?>
 					</ul>
 
 					<div class="w-full">
@@ -399,12 +375,7 @@ if (isset($_SESSION['email']) && isset($_SESSION['password'])) {
 						<div class="header-cart-buttons flex-w w-full">
 							<a href="shoping-cart.php"
 								class="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-r-8 m-b-10">
-								View Cart
-							</a>
-
-							<a href="shoping-cart.php"
-								class="flex-c-m stext-101 cl0 size-107 bg3 bor2 hov-btn3 p-lr-15 trans-04 m-b-10">
-								Check Out
+								View Wishlist
 							</a>
 						</div>
 					</div>
