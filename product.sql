ca76432726954df026e7fee2d5ff29c0b9ec9035
Mohd Shahid <mohd47149@gmail.com>
2024-08-21 00:37:58 +0530

    database updated

--- product.sql
+++ product.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 20, 2024 at 06:52 PM
+-- Generation Time: Aug 20, 2024 at 09:07 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -62,7 +62,11 @@ CREATE TABLE `product_images` (
 INSERT INTO `product_images` (`pr_id`, `pr_imgs`) VALUES
 (10, '[\"41VhSSlalQL._SL1198_.jpg\",\"61E0Jzp-JVL._SY879_.jpg\",\"61zdJmxjmuL._SL1500_.jpg\"]'),
 (11, '[\"51YmKgWW8yL._SL1198_.jpg\",\"719gA5x-+eL._SY879_.jpg\",\"715m7v58JDL._SL1500_.jpg\"]'),
-(12, '[\"41557PpoauL.jpg\",\"31lRz5TGgwL.jpg\",\"61dUjRK9hlL._SY575_.jpg\"]');
+(12, '[\"41557PpoauL.jpg\",\"31lRz5TGgwL.jpg\",\"61dUjRK9hlL._SY575_.jpg\"]'),
+(13, '[\"71Sz0Z8pN3L._SL1500_.jpg\",\"81YevpdhIFL._SL1500_.jpg\",\"71Y65B456dL._SL1500_.jpg\"]'),
+(14, '[\"61zi7stGeBL._SY879_.jpg\",\"71po53JscwL._SY879_.jpg\",\"71WN+O34voL._SY879_.jpg\"]'),
+(15, '[\"51LekMMIUTL._SX679_.jpg\",\"61STWDiipfL.jpg\",\"61wH1og5TrL._SX679_.jpg\"]'),
+(16, '[\"71sWDULjp1L._SL1500_.jpg\",\"718YrDupr0L._SL1500_.jpg\",\"61HGSEvjMyL._SL1200_.jpg\"]');
 
 -- --------------------------------------------------------
 
@@ -88,7 +92,11 @@ CREATE TABLE `product_item` (
 INSERT INTO `product_item` (`id`, `product_name`, `product_img`, `product_price`, `gender`, `product_description`, `product_related_img`, `product_catg`) VALUES
 (10, 'Lymio Men T-Shirt || T-Shirt for Men || Polo T Shirt || T-Shirt (polo-30-33)', '61J70J3DBwL._SY879_.jpg', '1299', 'M', 'Men T-Shirt || T-Shirt for Men || Polo T Shirt || T-Shirt\r\nPattern Type: Plain\r\nSleeve Length: Short Sleeve\r\nColor Disclaimer: Product color might slightly vary due to photographic lighting sources or your monitor settings\r\nFit type-Regular Fit', 10, 1),
 (11, 'Lux Cozi Mens Regular Fit Polo Neck Half Sleeve Solid Casual T-Shirt', '718j9P1Gf4L._SY879_.jpg', '1498', 'M', 'Lux Cozi Mens Regular Fit Polo Neck Half Sleeve Solid Casual T-shirt is a stylish and comfortable choice for casual wear.\r\nThe half sleeves offer a relaxed and versatile style, perfect for warmer weather or layering under jackets.\r\nThe solid color design adds a touch of elegance and makes it easy to pair with different bottoms.\r\nIt is suitable for various occasions, including casual outings, social gatherings, and relaxed work environments.\r\nThe T-shirt is designed with a classic polo neck, giving it a sophisticated and timeless look.', 11, 1),
-(12, 'Fox Racing uniquesex-adult Modern', '31WPIRNSGQL.jpg', '49999', 'M', 'Dual BOA Li2 system provides on the move micro-adjustability and improved power transfer to the pedals.\r\nOne-piece welded seamless upper reduces weight. Molded toe cap is designed to protect against rock strikes. Molded internal stiffening plate optimizes power transfer and off-bike comfort. and offers a precise fit.\r\nUltratac rubber compound provides excellent durability and unprecedented grip.\r\nHigh and low arch support options for a custom fit.\r\n2-bolt cleat system is compatible with all major pedal suppliers.', 12, 3);
+(12, 'Fox Racing uniquesex-adult Modern', '31WPIRNSGQL.jpg', '49999', 'M', 'Dual BOA Li2 system provides on the move micro-adjustability and improved power transfer to the pedals.\r\nOne-piece welded seamless upper reduces weight. Molded toe cap is designed to protect against rock strikes. Molded internal stiffening plate optimizes power transfer and off-bike comfort. and offers a precise fit.\r\nUltratac rubber compound provides excellent durability and unprecedented grip.\r\nHigh and low arch support options for a custom fit.\r\n2-bolt cleat system is compatible with all major pedal suppliers.', 12, 3),
+(13, 'Lymio Men T-Shirt || T-Shirt for Men || Plain T Shirt || T-Shirt (Polo-18-21)', '71Jukhvw8DL._SL1500_.jpg', '799', 'M', 'Men T Shirt || T-Shirt For Men || Plain T Shirt || T-Shirt\r\nPattern Type: Plain\r\nSleeve Length: Short Sleeve\r\nColor Disclaimer: Product color might slightly vary due to photographic lighting sources or your monitor settings\r\nFit type-Regular Fit', 13, 1),
+(14, 'Lymio Men T-Shirt || Regular Fit T-Shirt for Men || Plain T Shirt || T-Shirt (Polo-11-13)', '71DGe7O4jCL._SY879_.jpg', '499', 'M', 'Men T Shirt || T-Shirt For Men || Plain T Shirt || T-Shirt\r\nPattern Type: Plain\r\nSleeve Length: Short Sleeve\r\nColor Disclaimer: Product color might slightly vary due to photographic lighting sources or your monitor settings', 14, 1),
+(15, 'Samsung Galaxy Watch4 Classic LTE (4.6cm, Black)', 'ab.jpg', '10999', 'M', 'Only compatible with Android Smartphones (Runs on Wear OS Powered by Samsung)\r\nBioelectrical Impedance Analysis Sensor for Body Composition Analysis, Optical Heart Rate Sensor.\r\nHealth Monitoring features such as Advanced Sleep Analysis & Women Health.', 15, 2),
+(16, 'OnePlus Watch 2 with Wear OS4,Snapdragon W5 Chipset,Upto 100 hrs Battery Life,1.43’’ AMOLED Display,', '71RYeHLHnGL._SL1500_.jpg', '23499', 'M', 'OS + Chipsets + Storage Wear OS 4, Snapdragon W5 + BES2700 dual chipsets with 2GB RAM and 32GB ROM memory;Battery Life + Fast Charging Up to 100 hours battery life in Smart Mode (Up to 48 hours with heavy use), and 12 days battery life in Power Saver Mode. Fully charged in 60 minutes with VOOC fast charging, and 24 hours of use with 10 minutes of charging.\r\n1.43\" AMOLED Display Default brightness of 600 nits, up to 1000 nits in High Brightness mode. 466*466 resolution with 326 PPI.;Design & Durability Stainless Steel chassis and Sapphire Crystal cover, Military grade MIL-STD-810H standard certified. 5 ATM + IP68 rated.', 16, 2);
 
 --
 -- Indexes for dumped tables
@@ -124,7 +132,7 @@ ALTER TABLE `product_item`
 -- AUTO_INCREMENT for table `product_item`
 --
 ALTER TABLE `product_item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- Constraints for dumped tables
