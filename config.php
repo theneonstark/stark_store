497eb492781842a642ff57b9d3169c5b70caaf48
Mohd Shahid <mohd47149@gmail.com>
2024-08-21 22:59:33 +0530

    add Feedback and query db in config

--- config.php
+++ config.php
@@ -2,4 +2,5 @@
   $con =mysqli_connect("localhost","root","","user");
   $product_info = mysqli_connect("localhost","root","","product");
   $wishlist_info = mysqli_connect("localhost","root","","wishlist");
+  $fandq_info = mysqli_connect("localhost","root","","fandq");
 ?>
