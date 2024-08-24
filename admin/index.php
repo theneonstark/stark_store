64fb62c5286daad454c93b87e0dbecd047fbc5a4
Shahid <mohd47149@gmail.com>
2024-08-24 17:32:37 +0530

    add profile page url

--- admin/index.php
+++ admin/index.php
@@ -20,7 +20,7 @@
 <?php
   session_start();
   include('../config.php');
-  if(isset($_SESSION['admin_email'])){
+  if(isset($_SESSION['email'])){ 
 
     $users_details = mysqli_query($con,"SELECT * FROM department right join users ON users.office = department.dept_id");
     $admins_details = mysqli_query($con,"SELECT * FROM admins left join department ON admins.id = department.dept_id");
@@ -383,7 +383,7 @@
               <button class="align-middle rounded-full focus:shadow-outline-purple focus:outline-none"
                 @click="toggleProfileMenu" @keydown.escape="closeProfileMenu" aria-label="Account" aria-haspopup="true">
                 <img class="object-cover w-8 h-8 rounded-full"
-                  src="./assets/img/<?php echo $_SESSION['admin_img']?>"
+                  src="./assets/img/<?php echo $_SESSION['profile_img']?>"
                   alt="" aria-hidden="true" />
               </button>
               <template x-if="isProfileMenuOpen">
@@ -393,7 +393,7 @@
                   aria-label="submenu">
                   <li class="flex">
                     <a class="inline-flex items-center w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
-                      href="#">
+                      href="profile.php">
                       <svg class="w-4 h-4 mr-3" aria-hidden="true" fill="none" stroke-linecap="round"
                         stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
                         <path d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"></path>
