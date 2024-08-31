10ad0ada017054ea1e2821bf364e4bb17dd61bb9
Mohd Shahid <mohd47149@gmail.com>
2024-08-31 23:20:28 +0530

    some small bucks fix

--- js/main.js
+++ js/main.js
@@ -224,7 +224,7 @@
     [ +/- num product ]*/
     $('.btn-num-product-down').on('click', function(){
         var numProduct = Number($(this).next().val());
-        if(numProduct > 0) {
+        if(numProduct > 1) {
             $(this).next().val(numProduct - 1);
             numProduct--;
         }
