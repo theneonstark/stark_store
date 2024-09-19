c3193db9ab3ca30ed60a6ecb0ae1a0e81b8cfb6c
Mohd Shahid <mohd47149@gmail.com>
2024-09-19 23:47:19 +0530

    complete data fetch in checkout page

--- checkout.php
+++ checkout.php
@@ -404,15 +404,15 @@ include('config.php');
 			foreach ($product_ids as $index => $product_id) {
 				$cart_details = mysqli_query($product_info, "SELECT * FROM product_item WHERE id = $product_id");
 				while($checkout_data = mysqli_fetch_assoc($cart_details)){
-				echo "Product ID: ". $product_id . "<br>";
-				echo "Price: ₹ " . $product_prices[$index] . "<br>";
 	?>
 		<div class="flex flex-col rounded-lg bg-white sm:flex-row">
-			<img class="m-2 h-24 w-28 rounded-md border object-cover object-center" src="https://images.unsplash.com/flagged/photo-1556637640-2c80d3201be8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8c25lYWtlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+			<div class="m-2 h-24 w-28 rounded-md border object-cover object-center">
+				<img class="w-auto h-auto" src="image/product/<?php echo $checkout_data['product_img']?>" alt="" />
+			</div>
 			<div class="flex w-full flex-col px-4 py-4">
 			<span class="font-semibold"><?php echo $checkout_data['product_name']?></span>
-			<span class="float-right text-gray-400">42EU - 8.5US</span>
-			<p class="text-lg font-bold">$138.99</p>
+			<!-- <span class="float-right text-gray-400">42EU - 8.5US</span> -->
+			<p class="text-lg font-bold"><?php echo $product_prices[$index]?></p>
 			</div>
 		</div>
 
