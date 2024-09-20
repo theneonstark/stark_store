058a9310b8548ab527372aee272b59c9ef646047
Shahid <mohd47149@gmail.com>
2024-09-20 04:37:40 -0700

    user db updated

--- user.sql
+++ user.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 22, 2024 at 08:56 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Generation Time: Sep 20, 2024 at 01:36 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -78,30 +78,34 @@ CREATE TABLE `users` (
   `Mobile` varchar(100) NOT NULL,
   `password` varchar(150) NOT NULL,
   `profile_img` text DEFAULT 'user_profile.jpg',
-  `department` int(11) DEFAULT 2,
   `office` int(11) DEFAULT 2,
   `username` text DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `wishlist` varchar(100) DEFAULT 'user_wishlist',
-  `cart` varchar(100) DEFAULT 'user_cart'
+  `cart` varchar(100) DEFAULT 'user_cart',
+  `address` text DEFAULT NULL,
+  `landmark` text DEFAULT NULL,
+  `city` text DEFAULT NULL,
+  `zip` varchar(50) DEFAULT NULL,
+  `state` varchar(50) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `Mobile`, `password`, `profile_img`, `department`, `office`, `username`, `created_at`, `updated_at`, `wishlist`, `cart`) VALUES
-(11, 'Tarun', 'tarun@gmail.com', '9632587410', 'tarun123', 'user_profile.jpg', 2, 2, 'Tartarunta96', '2024-08-10 07:51:05', '2024-08-10 18:57:37', '11Tarun_wishlist', '11Tarun'),
-(12, 'Sahil', 'sahil@gmail.com', '7418529630', 'sahil123', 'user_profile.jpg', 2, 2, 'Sahsahilsa74', '2024-08-10 08:07:27', '2024-08-11 16:07:27', '12Sahil_wishlist', '12Sahil_cart'),
-(13, 'Manish', 'manish@gmail.com', '8526541254', 'manish123', 'user_profile.jpg', 2, 2, 'Manmanisma85', '2024-08-10 08:13:23', '2024-08-11 17:29:58', '13Manish_wishlist', '13Manish_cart'),
-(14, 'Punit', 'punit@gmail.com', '9856745872', 'pun123', 'user_profile.jpg', 2, 2, 'Punpunitpu98', '2024-08-10 18:35:06', '2024-08-10 18:58:12', '14Punit_wishlist', '14Punit_cart'),
-(15, 'rahul', 'rahul@gmail.com', '8541236529', 'rah123', 'user_profile.jpg', 2, 2, 'rahrahulra85', '2024-08-10 18:42:34', '2024-08-10 18:44:41', '15rahul_wishlist', 'user_cart'),
-(16, 'zara', 'zara@gmail.com', '9652745285', 'zara123', 'user_profile.jpg', 2, 2, 'zarzara@za96', '2024-08-10 18:47:02', '2024-08-10 18:47:30', '16zara_wishlist', 'user_cart'),
-(17, 'Aysha', 'aysha@gmail.com', '9874123685', 'aysha123', 'user_profile.jpg', 2, 2, 'Aysayshaay98', '2024-08-10 18:49:22', '2024-08-11 16:08:24', '17Aysha_wishlist', '17Aysha_cart'),
-(18, 'Mohit', 'mohit@gmail.com', '7452145638', 'momo123', 'user_profile.jpg', 2, 2, 'Mohmohitmo74', '2024-08-21 18:33:30', '2024-08-21 18:33:30', 'user_wishlist', 'user_cart'),
-(19, 'Kunal', 'kunal@gmail.com', '7845212365', 'kun123', 'user_profile.jpg', 2, 2, 'Kunkunalku78', '2024-08-21 18:34:54', '2024-08-21 18:34:54', 'user_wishlist', 'user_cart'),
-(20, 'Gungun', 'gungun@gmail.com', '8965232145', 'gun009', 'user_profile.jpg', 2, 2, 'Gungungugu89', '2024-08-21 18:35:41', '2024-08-21 18:35:41', 'user_wishlist', 'user_cart');
+INSERT INTO `users` (`id`, `name`, `email`, `Mobile`, `password`, `profile_img`, `office`, `username`, `created_at`, `updated_at`, `wishlist`, `cart`, `address`, `landmark`, `city`, `zip`, `state`) VALUES
+(11, 'Tarun', 'tarun@gmail.com', '9632587410', 'tarun123', 'user_profile.jpg', 2, 'Tartarunta96', '2024-08-10 07:51:05', '2024-08-10 18:57:37', '11Tarun_wishlist', '11Tarun', NULL, NULL, NULL, NULL, NULL),
+(12, 'Sahil', 'sahil@gmail.com', '7418529630', 'sahil123', 'user_profile.jpg', 2, 'Sahsahilsa74', '2024-08-10 08:07:27', '2024-08-11 16:07:27', '12Sahil_wishlist', '12Sahil_cart', NULL, NULL, NULL, NULL, NULL),
+(13, 'Manish', 'manish@gmail.com', '8526541254', 'manish123', 'user_profile.jpg', 2, 'Manmanisma85', '2024-08-10 08:13:23', '2024-08-11 17:29:58', '13Manish_wishlist', '13Manish_cart', NULL, NULL, NULL, NULL, NULL),
+(14, 'Punit', 'punit@gmail.com', '9856745872', 'pun123', 'user_profile.jpg', 2, 'Punpunitpu98', '2024-08-10 18:35:06', '2024-09-20 08:31:04', '14Punit_wishlist', '14Punit_cart', 'AB-145, Harsh Vihar, OM Nagar', 'Near GYM', 'Badarpur', '110044', 'New Delhi'),
+(15, 'rahul', 'rahul@gmail.com', '8541236529', 'rah123', 'user_profile.jpg', 2, 'rahrahulra85', '2024-08-10 18:42:34', '2024-08-10 18:44:41', '15rahul_wishlist', 'user_cart', NULL, NULL, NULL, NULL, NULL),
+(16, 'zara', 'zara@gmail.com', '9652745285', 'zara123', 'user_profile.jpg', 2, 'zarzara@za96', '2024-08-10 18:47:02', '2024-08-10 18:47:30', '16zara_wishlist', 'user_cart', NULL, NULL, NULL, NULL, NULL),
+(17, 'Aysha', 'aysha@gmail.com', '9874123685', 'aysha123', 'user_profile.jpg', 2, 'Aysayshaay98', '2024-08-10 18:49:22', '2024-08-11 16:08:24', '17Aysha_wishlist', '17Aysha_cart', NULL, NULL, NULL, NULL, NULL),
+(18, 'Mohit', 'mohit@gmail.com', '7452145638', 'momo123', 'user_profile.jpg', 2, 'Mohmohitmo74', '2024-08-21 18:33:30', '2024-08-21 18:33:30', 'user_wishlist', 'user_cart', NULL, NULL, NULL, NULL, NULL),
+(19, 'Kunal', 'kunal@gmail.com', '7845212365', 'kun123', 'user_profile.jpg', 2, 'Kunkunalku78', '2024-08-21 18:34:54', '2024-08-21 18:34:54', 'user_wishlist', 'user_cart', NULL, NULL, NULL, NULL, NULL),
+(20, 'Gungun', 'gungun@gmail.com', '8965232145', 'gun009', 'user_profile.jpg', 2, 'Gungungugu89', '2024-08-21 18:35:41', '2024-08-21 18:35:41', 'user_wishlist', 'user_cart', NULL, NULL, NULL, NULL, NULL);
 
 --
 -- Indexes for dumped tables
