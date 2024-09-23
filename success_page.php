e66105cae32790609a5999bcbc54000a9c48be52
Shahid <mohd47149@gmail.com>
2024-09-23 04:55:52 -0700

    authentication of google verify with db connect complete

--- success_page.php
+++ success_page.php
@@ -2,7 +2,7 @@
 include('config.php');
 session_start();
 if (!isset($_SESSION['google_loggedin'])) {
-    header('Location: login.php');
+    header('Location: signup.php');
     exit;
 }
 // Retrieve session variables
@@ -19,18 +19,19 @@ $pic = $full_details['picture'];
 $email = $full_details['email'];
 $email_v = $full_details['email_verified'];
 
-$check_user = $con->prepare("SELECT COUNT(*) FROM google_user WHERE email = ? OR sub = ?");
+$check_user = $con->prepare("SELECT * FROM google_user WHERE email = ? OR sub = ?");
     $check_user->bind_param("ss", $email, $sub);
     $check_user->execute();
-    $check_user->bind_result($count);
-    $check_user->fetch();
-    $check_user->close();
-if(!$count > 0){
-    $g_user = $con->prepare("INSERT INTO google_user (sub,name,given_name,picture,email,email_verify) VALUES (?,?,?,?,?,?)");
-    $g_user->bind_param("ssssss", $sub, $name, $given, $pic, $email, $email_v);
-    $g_user->execute();
+    $google_user_result = $check_user->get_result();
+if($google_user_result->num_row){
+    // $g_user = $con->prepare("INSERT INTO google_user (sub,name,given_name,picture,email,email_verify) VALUES (?,?,?,?,?,?)");
+    // $g_user->bind_param("ssssss", $sub, $name, $given, $pic, $email, $email_v);
+    // $g_user->execute();
+    echo "Data matched";
+    header('location: signup1.php');
 }else{
     echo "user already exsist";
+    header('location: signup.php');
 }
     
     
