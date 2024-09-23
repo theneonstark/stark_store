279293a6fb9c5957cb7b29ff9f5ec63f960829a8
Shahid <mohd47149@gmail.com>
2024-09-23 02:28:39 -0700

    google auth complete

---
+++ google_auth.php
@@ -0,0 +1,84 @@
+<?php
+session_start();
+// 778357140705-e2be615r2qhpf198er0fa5bgcm1ppi1u.apps.googleusercontent.com
+// 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
+$google_oauth_client_id = 'YOUR_ID';
+$google_oauth_client_secret = 'YOUR_SECRET';
+$google_oauth_redirect_uri = 'http://localhost/stark_store/google_auth.php';
+$google_oauth_version = 'v3';
+
+if (isset($_GET['code']) && !empty($_GET['code'])) {
+    $params = [
+        'code' => $_GET['code'],
+        'client_id' => $google_oauth_client_id,
+        'client_secret' => $google_oauth_client_secret,
+        'redirect_uri' => $google_oauth_redirect_uri,
+        'grant_type' => 'authorization_code'
+    ];
+    
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://oauth2.googleapis.com/token');
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    echo "<pre>";
+    $response = curl_exec($ch);
+    
+    if (curl_errno($ch)) {
+        exit('cURL error: ' . curl_error($ch));
+    }
+
+    curl_close($ch);
+    $response = json_decode($response, true);
+    print_r($response);
+
+    if (isset($response['access_token']) && !empty($response['access_token'])) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://www.googleapis.com/oauth2/' . $google_oauth_version . '/userinfo');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: Bearer ' . $response['access_token']]);
+        
+        $user_info_response = curl_exec($ch);
+        
+        if (curl_errno($ch)) {
+            exit('cURL error: ' . curl_error($ch));
+        }
+
+        curl_close($ch);
+        $profile = json_decode($user_info_response, true);
+        print_r($profile);
+        
+        if (isset($profile['email'])) {
+            $google_name_parts = [];
+            $google_name_parts[] = isset($profile['given_name']) ? preg_replace('/[^a-zA-Z0-9]/s', '', $profile['given_name']) : '';
+            $google_name_parts[] = isset($profile['family_name']) ? preg_replace('/[^a-zA-Z0-9]/s', '', $profile['family_name']) : '';
+
+            session_regenerate_id();
+            $_SESSION['full_details'] = $profile;
+            $_SESSION['google_loggedin'] = TRUE;
+            $_SESSION['google_email'] = $profile['email'];
+            $_SESSION['google_name'] = implode(' ', $google_name_parts);
+            $_SESSION['google_picture'] = isset($profile['picture']) ? $profile['picture'] : '';
+            
+            header('Location: success_page.php');
+            exit;
+        } else {
+            exit('Could not retrieve profile information! Please try again later!');
+        }
+    } else {
+        exit('Invalid access token! Please try again later!');
+    }
+} else {
+    $params = [
+        'response_type' => 'code',
+        'client_id' => $google_oauth_client_id,
+        'redirect_uri' => $google_oauth_redirect_uri,
+        'scope' => 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
+        'access_type' => 'offline',
+        'prompt' => 'consent'
+    ];
+    header('Location: https://accounts.google.com/o/oauth2/auth?' . http_build_query($params));
+    exit;
+}
+
+?>
