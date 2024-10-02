1f29486576437601bed35e5edd420efc9ef7927e
Mohd Shahid <mohd47149@gmail.com>
2024-10-02 15:46:36 +0530

    add order_detail page and POST data

--- order_details.php
+++ order_details.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <?php
-        include("config.php");
-        session_start();
-    ?>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="icon" type="image/png" href="images/icons/favicon.png" />
+	<?php
+	include("config.php");
+	session_start();
+	?>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="icon" type="image/png" href="images/icons/favicon.png" />
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
@@ -22,8 +23,9 @@
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<script src="https://cdn.tailwindcss.com"></script>
 </head>
+
 <body>
-<header class="header-v4">
+	<header class="header-v4">
 		<!-- Header desktop -->
 		<div class="container-menu-desktop">
 			<!-- Topbar -->
@@ -389,145 +391,152 @@
 			</div>
 		</div>
 	</div>
-    <div class="container-fluid">
+	<!-- order-details -->
+	 <?php
+	 	if(isset($_GET['id']) && isset($_GET['order_id'])){
+			echo $_GET['id'];
+			echo $_GET['order_id'];
+		}
+	 ?>
+	<div class="container-fluid">
 
-<div class="container">
-  <!-- Title -->
-  <div class="d-flex justify-content-between align-items-center py-3">
-    <h2 class="h5 mb-0"><a href="#" class="text-muted"></a> Order #16123222</h2>
-  </div>
+		<div class="container">
+			<!-- Title -->
+			<div class="d-flex justify-content-between align-items-center py-3">
+				<h2 class="h5 mb-0"><a href="#" class="text-muted"></a> Order #16123222</h2>
+			</div>
 
-  <!-- Main content -->
-  <div class="row">
-    <div class="col-lg-8">
-      <!-- Details -->
-      <div class="card mb-4">
-        <div class="card-body">
-          <div class="mb-3 d-flex justify-content-between">
-            <div>
-              <span class="me-3">22-11-2021</span>
-              <span class="me-3">#16123222</span>
-              <span class="me-3">Visa -1234</span>
-              <span class="badge rounded-pill bg-info">SHIPPING</span>
-            </div>
-            <div class="d-flex">
-              <button class="btn btn-link p-0 me-3 d-none d-lg-block btn-icon-text"><i class="bi bi-download"></i> <span class="text">Invoice</span></button>
-              <div class="dropdown">
-                <button class="btn btn-link p-0 text-muted" type="button" data-bs-toggle="dropdown">
-                  <i class="bi bi-three-dots-vertical"></i>
-                </button>
-                <ul class="dropdown-menu dropdown-menu-end">
-                  <li><a class="dropdown-item" href="#"><i class="bi bi-pencil"></i> Edit</a></li>
-                  <li><a class="dropdown-item" href="#"><i class="bi bi-printer"></i> Print</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
-          <table class="table table-borderless">
-            <tbody>
-              <tr>
-                <td>
-                  <div class="d-flex mb-2">
-                    <div class="flex-shrink-0">
-                      <img src="https://www.bootdey.com/image/280x280/87CEFA/000000" alt="" width="35" class="img-fluid">
-                    </div>
-                    <div class="flex-lg-grow-1 ms-3">
-                      <h6 class="small mb-0"><a href="#" class="text-reset">Wireless Headphones with Noise Cancellation Tru Bass Bluetooth HiFi</a></h6>
-                      <span class="small">Color: Black</span>
-                    </div>
-                  </div>
-                </td>
-                <td>1</td>
-                <td class="text-end">$79.99</td>
-              </tr>
-              <tr>
-                <td>
-                  <div class="d-flex mb-2">
-                    <div class="flex-shrink-0">
-                      <img src="https://www.bootdey.com/image/280x280/FF69B4/000000" alt="" width="35" class="img-fluid">
-                    </div>
-                    <div class="flex-lg-grow-1 ms-3">
-                      <h6 class="small mb-0"><a href="#" class="text-reset">Smartwatch IP68 Waterproof GPS and Bluetooth Support</a></h6>
-                      <span class="small">Color: White</span>
-                    </div>
-                  </div>
-                </td>
-                <td>1</td>
-                <td class="text-end">$79.99</td>
-              </tr>
-            </tbody>
-            <tfoot>
-              <tr>
-                <td colspan="2">Subtotal</td>
-                <td class="text-end">$159,98</td>
-              </tr>
-              <tr>
-                <td colspan="2">Shipping</td>
-                <td class="text-end">$20.00</td>
-              </tr>
-              <tr>
-                <td colspan="2">Discount (Code: NEWYEAR)</td>
-                <td class="text-danger text-end">-$10.00</td>
-              </tr>
-              <tr class="fw-bold">
-                <td colspan="2">TOTAL</td>
-                <td class="text-end">$169,98</td>
-              </tr>
-            </tfoot>
-          </table>
-        </div>
-      </div>
-      <!-- Payment -->
-      <div class="card mb-4">
-        <div class="card-body">
-          <div class="row">
-            <div class="col-lg-6">
-              <h3 class="h6">Payment Method</h3>
-              <p>Visa -1234 <br>
-              Total: $169,98 <span class="badge bg-success rounded-pill">PAID</span></p>
-            </div>
-            <div class="col-lg-6">
-              <h3 class="h6">Billing address</h3>
-              <address>
-                <strong>John Doe</strong><br>
-                1355 Market St, Suite 900<br>
-                San Francisco, CA 94103<br>
-                <abbr title="Phone">P:</abbr> (123) 456-7890
-              </address>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-4">
-      <!-- Customer Notes -->
-      <div class="card mb-4">
-        <div class="card-body">
-          <h3 class="h6">Customer Notes</h3>
-          <p>Sed enim, faucibus litora velit vestibulum habitasse. Cras lobortis cum sem aliquet mauris rutrum. Sollicitudin. Morbi, sem tellus vestibulum porttitor.</p>
-        </div>
-      </div>
-      <div class="card mb-4">
-        <!-- Shipping information -->
-        <div class="card-body">
-          <h3 class="h6">Shipping Information</h3>
-          <strong>FedEx</strong>
-          <span><a href="#" class="text-decoration-underline" target="_blank">FF1234567890</a> <i class="bi bi-box-arrow-up-right"></i> </span>
-          <hr>
-          <h3 class="h6">Address</h3>
-          <address>
-            <strong>John Doe</strong><br>
-            1355 Market St, Suite 900<br>
-            San Francisco, CA 94103<br>
-            <abbr title="Phone">P:</abbr> (123) 456-7890
-          </address>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>
-  </div>
-    <footer class="bg3 p-t-75 p-b-32">
+			<!-- Main content -->
+			<div class="row">
+				<div class="col-lg-8">
+					<!-- Details -->
+					<div class="card mb-4">
+						<div class="card-body">
+							<div class="mb-3 d-flex justify-content-between">
+								<div>
+									<span class="me-3">22-11-2021</span>
+									<span class="me-3">#16123222</span>
+									<span class="me-3">Visa -1234</span>
+									<span class="badge rounded-pill bg-info">SHIPPING</span>
+								</div>
+								<div class="d-flex">
+									<button class="btn btn-link p-0 me-3 d-none d-lg-block btn-icon-text"><i class="bi bi-download"></i> <span class="text">Invoice</span></button>
+									<div class="dropdown">
+										<button class="btn btn-link p-0 text-muted" type="button" data-bs-toggle="dropdown">
+											<i class="bi bi-three-dots-vertical"></i>
+										</button>
+										<ul class="dropdown-menu dropdown-menu-end">
+											<li><a class="dropdown-item" href="#"><i class="bi bi-pencil"></i> Edit</a></li>
+											<li><a class="dropdown-item" href="#"><i class="bi bi-printer"></i> Print</a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<table class="table table-borderless">
+								<tbody>
+									<tr>
+										<td>
+											<div class="d-flex mb-2">
+												<div class="flex-shrink-0">
+													<img src="https://www.bootdey.com/image/280x280/87CEFA/000000" alt="" width="35" class="img-fluid">
+												</div>
+												<div class="flex-lg-grow-1 ms-3">
+													<h6 class="small mb-0"><a href="#" class="text-reset">Wireless Headphones with Noise Cancellation Tru Bass Bluetooth HiFi</a></h6>
+													<span class="small">Color: Black</span>
+												</div>
+											</div>
+										</td>
+										<td>1</td>
+										<td class="text-end">$79.99</td>
+									</tr>
+									<tr>
+										<td>
+											<div class="d-flex mb-2">
+												<div class="flex-shrink-0">
+													<img src="https://www.bootdey.com/image/280x280/FF69B4/000000" alt="" width="35" class="img-fluid">
+												</div>
+												<div class="flex-lg-grow-1 ms-3">
+													<h6 class="small mb-0"><a href="#" class="text-reset">Smartwatch IP68 Waterproof GPS and Bluetooth Support</a></h6>
+													<span class="small">Color: White</span>
+												</div>
+											</div>
+										</td>
+										<td>1</td>
+										<td class="text-end">$79.99</td>
+									</tr>
+								</tbody>
+								<tfoot>
+									<tr>
+										<td colspan="2">Subtotal</td>
+										<td class="text-end">$159,98</td>
+									</tr>
+									<tr>
+										<td colspan="2">Shipping</td>
+										<td class="text-end">$20.00</td>
+									</tr>
+									<tr>
+										<td colspan="2">Discount (Code: NEWYEAR)</td>
+										<td class="text-danger text-end">-$10.00</td>
+									</tr>
+									<tr class="fw-bold">
+										<td colspan="2">TOTAL</td>
+										<td class="text-end">$169,98</td>
+									</tr>
+								</tfoot>
+							</table>
+						</div>
+					</div>
+					<!-- Payment -->
+					<div class="card mb-4">
+						<div class="card-body">
+							<div class="row">
+								<div class="col-lg-6">
+									<h3 class="h6">Payment Method</h3>
+									<p>Visa -1234 <br>
+										Total: $169,98 <span class="badge bg-success rounded-pill">PAID</span></p>
+								</div>
+								<div class="col-lg-6">
+									<h3 class="h6">Billing address</h3>
+									<address>
+										<strong>John Doe</strong><br>
+										1355 Market St, Suite 900<br>
+										San Francisco, CA 94103<br>
+										<abbr title="Phone">P:</abbr> (123) 456-7890
+									</address>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="col-lg-4">
+					<!-- Customer Notes -->
+					<div class="card mb-4">
+						<div class="card-body">
+							<h3 class="h6">Customer Notes</h3>
+							<p>Sed enim, faucibus litora velit vestibulum habitasse. Cras lobortis cum sem aliquet mauris rutrum. Sollicitudin. Morbi, sem tellus vestibulum porttitor.</p>
+						</div>
+					</div>
+					<div class="card mb-4">
+						<!-- Shipping information -->
+						<div class="card-body">
+							<h3 class="h6">Shipping Information</h3>
+							<strong>FedEx</strong>
+							<span><a href="#" class="text-decoration-underline" target="_blank">FF1234567890</a> <i class="bi bi-box-arrow-up-right"></i> </span>
+							<hr>
+							<h3 class="h6">Address</h3>
+							<address>
+								<strong>John Doe</strong><br>
+								1355 Market St, Suite 900<br>
+								San Francisco, CA 94103<br>
+								<abbr title="Phone">P:</abbr> (123) 456-7890
+							</address>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<footer class="bg3 p-t-75 p-b-32">
 		<div class="container">
 			<div class="row">
 				<div class="col-sm-6 col-lg-3 p-b-50">
@@ -679,13 +688,13 @@
 		</div>
 	</footer>
 
-    <div class="btn-back-to-top" id="myBtn">
+	<div class="btn-back-to-top" id="myBtn">
 		<span class="symbol-btn-back-to-top">
 			<i class="zmdi zmdi-chevron-up"></i>
 		</span>
 	</div>
 
-    <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+	<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
 	<script src="vendor/animsition/js/animsition.min.js"></script>
 	<script src="vendor/bootstrap/js/popper.js"></script>
 	<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
@@ -731,4 +740,5 @@
 	</script>
 	<script src="js/main.js"></script>
 </body>
+
 </html>
