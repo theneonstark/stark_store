28a15d02a3db159c032cda72c330b38256d3826c
Mohd Shahid <mohd47149@gmail.com>
2024-10-02 22:25:54 +0530

    delete logic complete and bugs fixed

--- admin/index.php
+++ admin/index.php
@@ -29,8 +29,11 @@
     
     if(isset($_POST['delete'])){
       $delete_id = $_POST['delete'];
-      $delete_table_query = mysqli_query($user_info, "drop table $delete_id");
-      $del_row = mysqli_query($con, "delete from users where username = '$delete_id'");
+      $delete_cart = $_POST['cart'];
+      $delete_wishlist = $_POST['wishlist'];
+      $delete_cart_table = mysqli_query($cart_info, "DROP TABLE IF EXISTS $delete_cart");
+      $delete_wishlist_table = mysqli_query($wishlist_info, "DROP TABLE IF EXISTS $delete_wishlist");
+      $del_row = mysqli_query($con, "DELETE FROM users WHERE id = '$delete_id'");
       if($del_row){
         echo "<script>alert('User deleted successfully')</script>";
       }else{
@@ -668,15 +671,19 @@
                           </svg>
                         </button>
                         </a>
+                        <form method="POST">
+                          <input type="text" value="<?php echo $person['cart']?>" name="cart">
+                          <input type="text" value="<?php echo $person['wishlist']?>" name="wishlist">
                         <button
                           class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
-                          aria-label="Delete" value="<?php echo $person['username']?>" name="delete">
+                          aria-label="Delete" value="<?php echo $person['id']?>" name="delete">
                           <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
                             <path fill-rule="evenodd"
                               d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
                               clip-rule="evenodd"></path>
                           </svg>
                         </button>
+                        </form>
                       </div>
                     </td>
                   </tr>
