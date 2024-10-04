0d85f53c2b818232c208dd25605833ed1768d649
Shahid <mohd47149@gmail.com>
2024-10-04 01:52:15 -0700

    change the link href for post

--- admin/order_data.php
+++ admin/order_data.php
@@ -601,8 +601,8 @@ if (isset($_SESSION['email'])) {
                           <?php echo $order_row['amount'] ?>
                         </td>
                         <td class="px-4 py-3 text-sm w-5 truncate">
-                          <!-- <?php echo $order_row['product_id']; ?> -->
-                          <a href="order_product_details.php?orders=<?php print_r($product_ids); ?>" class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                          <?php echo $order_row['product_id']; ?>
+                          <a href="order_product_details.php?orders=<?php echo $order_row['user_id'] ?>" class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
                             aria-label="Edit">
                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
                               <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z"></path>
