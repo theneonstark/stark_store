d6017c9203c193cd61c51de6b14b0699fd4ef51c
Shahid <shahid@gmail.com>
2024-10-08 14:28:50 +0530

    test again

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ if (isset($_SESSION['email']) || isset($_SESSION['google_email'])) {
 ?>
 
 	<head>
-		<title>Home - Stark Store</title>
+		<title>Home - Stark Store </title>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="icon" type="image/png" href="images/icons/favicon.png" />
