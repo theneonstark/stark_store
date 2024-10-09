92219bb6052fc3e28a196a6b6235c8996bc4937b
Mohd Shahid <mohd47149@gmail.com>
2024-10-09 07:15:19 +0530

    config for send notification

---
+++ notification-send.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+require_once "config.php";
+$name = $_SESSION['name'];
+$email = $_SESSION['email'];
+$username = $_SESSION['username'];
+$img = $_SESSION['profile_img'];
+
+$notification = mysqli_query($notification, "INSERT INTO admin_notify (notify_name, notify_email, notify_username, notify_img) VALUES ()");
