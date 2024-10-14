b6f1fc617f51045f4de95c9a34f28d905268e136
Mohd Shahid <mohd47149@gmail.com>
2024-10-15 00:18:02 +0530

    fixed the query

--- notification-send.php
+++ notification-send.php
@@ -6,5 +6,6 @@ $email = $_SESSION['email'];
 $username = $_SESSION['username'];
 $img = $_SESSION['profile_img'];
 
-$notification = mysqli_query($notification, "INSERT INTO admin_notify (notify_name, notify_email, notify_username, notify_img,active) VALUES ('$name',' $email', '$username', '$img', '1')");
+$notification = mysqli_query($notification, "INSERT INTO admin_notify (notify_name, notify_email, notify_username, notify_img,active, notify_active) VALUES ('$name',' $email', '$username', '$img', '1', '1')");
+header('location: index.php');
 ?>
