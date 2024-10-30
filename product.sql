afae548f348556c5e8cce7973895eec4a1f25603
Mohd Shahid <mohd47149@gmail.com>
2024-10-31 00:13:45 +0530

    update DB

--- product.sql
+++ product.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 20, 2024 at 09:07 PM
+-- Generation Time: Oct 30, 2024 at 07:42 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -66,7 +66,13 @@ INSERT INTO `product_images` (`pr_id`, `pr_imgs`) VALUES
 (13, '[\"71Sz0Z8pN3L._SL1500_.jpg\",\"81YevpdhIFL._SL1500_.jpg\",\"71Y65B456dL._SL1500_.jpg\"]'),
 (14, '[\"61zi7stGeBL._SY879_.jpg\",\"71po53JscwL._SY879_.jpg\",\"71WN+O34voL._SY879_.jpg\"]'),
 (15, '[\"51LekMMIUTL._SX679_.jpg\",\"61STWDiipfL.jpg\",\"61wH1og5TrL._SX679_.jpg\"]'),
-(16, '[\"71sWDULjp1L._SL1500_.jpg\",\"718YrDupr0L._SL1500_.jpg\",\"61HGSEvjMyL._SL1200_.jpg\"]');
+(16, '[\"71sWDULjp1L._SL1500_.jpg\",\"718YrDupr0L._SL1500_.jpg\",\"61HGSEvjMyL._SL1200_.jpg\"]'),
+(17, '[\"61gRzURpwPL._SX385_.jpg\",\"61k-Up1smUL.jpg\",\"617aCMTyxVL._SX342_.jpg\"]'),
+(18, '[\"41O5h3w01bL.jpg\",\"61kVB5UhudL._SY741_.jpg\",\"81-22a+3KJL._SY741_.jpg\"]'),
+(19, '[\"81edCHW+MlL._SX679_.jpg\",\"41y0wxhknBL.jpg\",\"61oAd7c6svL._SX342_.jpg\"]'),
+(20, '[\"91CfNNPQZcL._SY741_.jpg\",\"61S9EPYVbrL._SY741_.jpg\",\"614RgcJkQAL._SY445_.jpg\"]'),
+(21, '[\"614u99dX+HL._SL1500_.jpg\",\"61C8JNTZQdL._SL1500_.jpg\",\"61xfIdV7maL._SL1500_.jpg\"]'),
+(23, '[\"61-p5iMBC5L._SL1500_.jpg\",\"61lEfUv8gbL._SL1500_.jpg\",\"71UMrTcV3NL._SL1500_.jpg\"]');
 
 -- --------------------------------------------------------
 
@@ -96,7 +102,13 @@ INSERT INTO `product_item` (`id`, `product_name`, `product_img`, `product_price`
 (13, 'Lymio Men T-Shirt || T-Shirt for Men || Plain T Shirt || T-Shirt (Polo-18-21)', '71Jukhvw8DL._SL1500_.jpg', '799', 'M', 'Men T Shirt || T-Shirt For Men || Plain T Shirt || T-Shirt\r\nPattern Type: Plain\r\nSleeve Length: Short Sleeve\r\nColor Disclaimer: Product color might slightly vary due to photographic lighting sources or your monitor settings\r\nFit type-Regular Fit', 13, 1),
 (14, 'Lymio Men T-Shirt || Regular Fit T-Shirt for Men || Plain T Shirt || T-Shirt (Polo-11-13)', '71DGe7O4jCL._SY879_.jpg', '499', 'M', 'Men T Shirt || T-Shirt For Men || Plain T Shirt || T-Shirt\r\nPattern Type: Plain\r\nSleeve Length: Short Sleeve\r\nColor Disclaimer: Product color might slightly vary due to photographic lighting sources or your monitor settings', 14, 1),
 (15, 'Samsung Galaxy Watch4 Classic LTE (4.6cm, Black)', 'ab.jpg', '10999', 'M', 'Only compatible with Android Smartphones (Runs on Wear OS Powered by Samsung)\r\nBioelectrical Impedance Analysis Sensor for Body Composition Analysis, Optical Heart Rate Sensor.\r\nHealth Monitoring features such as Advanced Sleep Analysis & Women Health.', 15, 2),
-(16, 'OnePlus Watch 2 with Wear OS4,Snapdragon W5 Chipset,Upto 100 hrs Battery Life,1.43’’ AMOLED Display,', '71RYeHLHnGL._SL1500_.jpg', '23499', 'M', 'OS + Chipsets + Storage Wear OS 4, Snapdragon W5 + BES2700 dual chipsets with 2GB RAM and 32GB ROM memory;Battery Life + Fast Charging Up to 100 hours battery life in Smart Mode (Up to 48 hours with heavy use), and 12 days battery life in Power Saver Mode. Fully charged in 60 minutes with VOOC fast charging, and 24 hours of use with 10 minutes of charging.\r\n1.43\" AMOLED Display Default brightness of 600 nits, up to 1000 nits in High Brightness mode. 466*466 resolution with 326 PPI.;Design & Durability Stainless Steel chassis and Sapphire Crystal cover, Military grade MIL-STD-810H standard certified. 5 ATM + IP68 rated.', 16, 2);
+(16, 'OnePlus Watch 2 with Wear OS4,Snapdragon W5 Chipset,Upto 100 hrs Battery Life,1.43’’ AMOLED Display,', '71RYeHLHnGL._SL1500_.jpg', '23499', 'M', 'OS + Chipsets + Storage Wear OS 4, Snapdragon W5 + BES2700 dual chipsets with 2GB RAM and 32GB ROM memory;Battery Life + Fast Charging Up to 100 hours battery life in Smart Mode (Up to 48 hours with heavy use), and 12 days battery life in Power Saver Mode. Fully charged in 60 minutes with VOOC fast charging, and 24 hours of use with 10 minutes of charging.\r\n1.43\" AMOLED Display Default brightness of 600 nits, up to 1000 nits in High Brightness mode. 466*466 resolution with 326 PPI.;Design & Durability Stainless Steel chassis and Sapphire Crystal cover, Military grade MIL-STD-810H standard certified. 5 ATM + IP68 rated.', 16, 2),
+(17, 'Columbia Women\'s W Omni-Heat Infinity Knit LS Crew', '613WyitV6yL.jpg', '4999', 'F', 'Features: Omni-Heat Infinity advanced thermal reflective., Omni-Wick., Comfort stretch., Captures and neutralizes Odor for long-lasting freshness.', 17, 1),
+(18, 'Libas Purple Yoke Design Silk Blend Anarkali Kurta With Churidar & Dupatta', '61-COg8J9RL._SY606_.jpg', '2879', 'F', 'Top Color : Purple\r\nSet Contents : 1 Kurta 1 Churidar 1 Dupatta\r\nTop Pattern : Yoke Design\r\nTop Fabric :Silk Blend\r\nOccasion : Festive', 18, 1),
+(19, 'BIBA Women Rayon Printed Straight Kurta', '71ovBo96PEL._SX342_.jpg', '1479', 'F', 'Colour : Green\r\nKurta Fit : Straight\r\nWash Care : Wash Separately In Cold Water, Do Not Bleach, Dry In Shade, Medium To Hot Iron.,Made In India\r\nPackage Includes : Kurta\r\nDepartment : Women\r\nKurta', 19, 1),
+(20, 'W for Woman Solid Straight Kurta Sets With Slim Pants & Dupatta', '71RbvzziCrL._SY679_.jpg', '2200', 'F', 'Product Description: This off-white coloured viscose kurta set for women has straight silhouette and regular fit. The kurta is designed in round neck and has 3/4 sleeve. This beautiful solid kurta for women is crafted in rayon fabric which is comfy, stylish and add premium look. The set also includes slim pant, which is crafted in chanderi. This kurta set with a dupatta, which is adorned with solid and crafted in chanderi.\r\nFabric: This 80% viscose 20% man made fiber kurta for women is made with premium chanderi quality; slim pant is made with ps, dupatta/gilet is made with kota doria. The kurta set has long-time wearability and is friendly to skin.\r\nWash Care: This women kurta set is delicate washable and go-to summer pick.\r\nOccasion: It makes a great addition to your wardrobe for festive. This pure cotton kurta set is also a great pick for gifting purpose.\r\nStyle Suggestion: Elevate the kurta set with delicate silver jhumkas and embroidered white juttis for a timeless and elegant look.', 20, 1),
+(21, 'OnePlus Watch 2 with Wear OS4,Snapdragon W5 Chipset,Upto 100 hrs Battery Life,1.43’’ AMOLED Display,', '61DpOqDGGRL._SL1500_.jpg', '1299', 'O', 'A quick 10-minute charge delivers up to 20 hours of immersive audio playback; The flagship-level battery life delivers up to 30 hours of non-stop music on a single charge\r\nA large 12.4 mm bass driver delivers uncompromisingly deep bass for powerful beats. Experience incredibly rich audio detail at every frequency with the titanium coating dome. Codec: Supports AAC and SBC\r\nAnti-distortion audio technology ensures your audio playtime stays silky-smooth. Thanks to the larger sound cavity and industry-leading algorithms, notes remain distortion-free\r\nWater and sweat-resistant, the IP55-rated internals and design ensure your OnePlus Bullets Wireless Z2 stay all-weather ready\r\nBattery Average Life: 30.0 Hours; Model: E305a', 21, 5),
+(23, 'Samsung Galaxy Buds Fe (White)| Powerful Active Noise Cancellation |in Ear Enriched Bass Sound | Erg', '61dTcv+ma3L._SL1500_.jpg', '3999', 'O', 'SUPERIOR SOUND - Indulge in rich sound of Galaxy Buds FE with deep, powerful bass from the new 1-way speaker, no matter what you\'re listening to.\r\nNOISE OFF, YOUR WORLD IS ON - Active Noise Canceling instantly puts you alone with your music in a quiet space. Keep the distractions out and immerse yourself in the pure sound.\r\nINTUTIVE CONTROL - The clear distinction between the touch area and the grip area makes it easy for you to control your music and calls while keeping them securely in place.\r\nERGONOMIC DESIGN - For added comfort as they snugly nestle in your ears and stay securely in place. Customize your earbud fit with different size choices for both wing tips and ear tips.\r\nCOMPACT SIZE, YET POWERFUL - The battery lasts for up to 30 hours on a single charge, keeping up with your long playlist of songs, podcasts or social media videos throughout your day.', 23, 5);
 
 --
 -- Indexes for dumped tables
@@ -132,7 +144,7 @@ ALTER TABLE `product_item`
 -- AUTO_INCREMENT for table `product_item`
 --
 ALTER TABLE `product_item`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
 
 --
 -- Constraints for dumped tables
