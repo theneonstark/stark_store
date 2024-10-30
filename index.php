762ec7a38f9e7cb63ef5fc2b05a7143c673e4236
Mohd Shahid <mohd47149@gmail.com>
2024-10-31 00:07:16 +0530

    add new products and fixed some bugs

--- index.php
+++ index.php
@@ -420,7 +420,7 @@ $wishlist_data = "select * from wishlist";
 						<div class="block1 wrap-pic-w">
 							<img src="images/banner-03.jpg" alt="IMG-BANNER">
 
-							<a href="product.php?product_target=accessories"
+							<a href="product.php?product_target=O"
 								class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3">
 								<div class="block1-txt-child1 flex-col-l">
 									<span class="block1-name ltext-102 trans-04 p-b-8">
