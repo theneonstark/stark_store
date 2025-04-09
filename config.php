47acd25ff8d09ae7ad2c356c8874ec81fa600e9e
theneonstark <mohd47149@gmail.com>
2025-04-09 18:38:58 +0530

    change according to the server

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
-  $con =mysqli_connect("localhost","root","","user");
-  $product_info = mysqli_connect("localhost","root","","product");
-  $wishlist_info = mysqli_connect("localhost","root","","wishlist");
-  $fandq_info = mysqli_connect("localhost","root","","fandq");
-  $cart_info = mysqli_connect("localhost","root","","usercart");
-  $user_order = mysqli_connect("localhost","root","","user_order");
-  $notification = mysqli_connect("localhost","root","","notification");
-  $blog = mysqli_connect("localhost","root","","blogs");
+  $con =mysqli_connect("localhost","pehunt_member","BJ;RDe;0?[7V","pehunt_user");
+  $product_info = mysqli_connect("localhost","pehunt_member","BJ;RDe;0?[7V","pehunt_product");
+  $wishlist_info = mysqli_connect("localhost","pehunt_member","BJ;RDe;0?[7V","pehunt_wishlist");
+  $fandq_info = mysqli_connect("localhost","pehunt_member","BJ;RDe;0?[7V","pehunt_fandq");
+  $cart_info = mysqli_connect("localhost","pehunt_member","BJ;RDe;0?[7V","pehunt_usercart");
+  $user_order = mysqli_connect("localhost","pehunt_member","BJ;RDe;0?[7V","pehunt_user_order");
+  $notification = mysqli_connect("localhost","pehunt_member","BJ;RDe;0?[7V","pehunt_notification");
+  $blog = mysqli_connect("localhost","pehunt_member","BJ;RDe;0?[7V","pehunt_blogs");
 ?>
