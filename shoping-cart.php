b24932eb471b58f354e2c47f6da000bd954a3fbd
theneonstark <mohd47149@gmail.com>
2025-04-10 22:34:36 +0530

    changes for company requirement 9.6.2

--- shoping-cart.php
+++ shoping-cart.php
@@ -257,7 +257,7 @@ if (isset($_SESSION['email']) || isset($_SESSION['google_email'])) {
 	<form action="checkout.php" method="POST" class="bg0 p-t-75 p-b-85">
 		<?php
 		$cart_user = $_SESSION['cart'];
-		$cart_db = "usercart";
+		$cart_db = "pehunt_usercart";
 		// Prepare the SQL query
 		$cart_table_query = $con->prepare("SELECT COUNT(*) as count FROM information_schema.tables WHERE table_schema = ? AND table_name = ?");
 		?>
@@ -280,7 +280,7 @@ if (isset($_SESSION['email']) || isset($_SESSION['google_email'])) {
 								$cart_table_result = $cart_table_query->get_result();
 								$cart_table_row = $cart_table_result->fetch_assoc();
 								if ($cart_table_row['count'] > 0) {
-									$cart_details = mysqli_query($cart_info, "SELECT * FROM usercart.$cart_user uw JOIN product.product_item pi ON uw.cp_detail = pi.id WHERE uw.cp_detail AND pi.id ");
+									$cart_details = mysqli_query($cart_info, "SELECT * FROM pehunt_usercart.$cart_user uw JOIN pehunt_product.product_item pi ON uw.cp_detail = pi.id WHERE uw.cp_detail AND pi.id ");
 									while ($cart_fetch = mysqli_fetch_assoc($cart_details)) {
 								?>
 										<tr class="table_row">
