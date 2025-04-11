eb0063c731992b3aad24ea9131bd943082b34adf
theneonstark <mohd47149@gmail.com>
2025-04-11 15:22:58 +0530

    changes for company requirement 10.4.1

--- admin/user_profile_details.php
+++ admin/user_profile_details.php
@@ -14,10 +14,10 @@ if (isset($_SESSION['email'])) {
     <link rel="stylesheet" href="./assets/css/tailwind.output.css" />
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <script src="./assets/js/init-alpine.js"></script>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" defer></script>
+    <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" defer></script> -->
     <script src="./assets/js/charts-lines.js" defer></script>
-    <!-- <script src="./assets/js/charts-pie.js" defer></script> -->
+    <script src="./assets/js/charts-pie.js" defer></script>
     <script src="https://cdn.tailwindcss.com"></script>
   </head>
 
