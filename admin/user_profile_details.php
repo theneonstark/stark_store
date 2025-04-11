63810b4f552a386f9aacf80f9bdfb057dc2791c9
theneonstark <mohd47149@gmail.com>
2025-04-11 15:20:37 +0530

    changes for company requirement 10.4

--- admin/user_profile_details.php
+++ admin/user_profile_details.php
@@ -17,7 +17,7 @@ if (isset($_SESSION['email'])) {
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js" defer></script>
     <script src="./assets/js/charts-lines.js" defer></script>
-    <script src="./assets/js/charts-pie.js" defer></script>
+    <!-- <script src="./assets/js/charts-pie.js" defer></script> -->
     <script src="https://cdn.tailwindcss.com"></script>
   </head>
 
