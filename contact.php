9aac221f485d34909560d86b905b94b89a44c7ba
theneonstark <mohd47149@gmail.com>
2025-06-12 11:39:51 +0530

    add proper address

--- contact.php
+++ contact.php
@@ -317,7 +317,7 @@ $wishlist_data = "select * from wishlist";
 							</span>
 
 							<p class="stext-115 cl6 size-213 p-t-18">
-							Pehunt solution OPC Pvt Ltd , Office No GF-05, H73, Sector 63 Noida UP 201301
+							Pehunt solution OPC Pvt Ltd , Office No GF-05, H73, Gautambudha nagar, Sector 63 Noida UP 201301
 							</p>
 						</div>
 					</div>
@@ -443,7 +443,7 @@ $wishlist_data = "select * from wishlist";
 						</p>
 
 						<p class="stext-107 cl7 size-201">
-							Any questions? Let us know in store at Pehunt solution OPC Pvt Ltd , Office No GF-05, H73, Sector 63 Noida UP 201301
+							Any questions? Let us know in store at Pehunt solution OPC Pvt Ltd , Office No GF-05, H73, Gautambudha nagar, Sector 63 Noida UP 201301
 						</p>
 						<li>
 								<a href="about.php">About</a>
